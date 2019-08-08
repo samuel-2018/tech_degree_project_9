4a6eb027f60b80b1c66b8d54b2678f5be77b75bc
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-08-08 08:26:12 -0400

    added user authentication and hashing of passwords

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,6 @@
+// Enables hashing of passwords
+const bcryptjs = require('bcryptjs');
+
 const express = require('express');
 
 const router = express.Router();
@@ -10,16 +13,34 @@ const { User } = models;
 // Sequelize operators
 const { Op } = sequelize;
 
+// ========================================
+//  HELPER FUNCTIONS
+// ========================================
+const authenticateUser = require('../helpers/authenticateUser');
+// ========================================
+// ROUTES
+// ========================================
+
 router
   .route('/')
   // Get current authenticated user
-  .get(async (req, res, next) => {
+  .get(authenticateUser, async (req, res, next) => {
     //
+    const user = await req.currentUser;
+
+    res.json({ user });
   })
   // Create user
   .post(async (req, res, next) => {
     try {
-      await User.create(req.body);
+      // Builds new user
+      const newUser = await User.build(req.body);
+
+      // Hashes password
+      newUser.password = await bcryptjs.hashSync(newUser.password);
+
+      // Save
+      await newUser.save(req.body);
 
       res.writeHead(201, {
         Location: '/',

7329695b2dd9941cea3693779bf37bbc07aee0f8
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-08-08 17:10:11 -0400

    errors handled by next(),  fixed update bug

--- routes/courses.js
+++ routes/courses.js
@@ -16,6 +16,11 @@ const { Op } = sequelize;
 //  HELPER FUNCTIONS
 // ========================================
 const authenticateUser = require('../helpers/authenticateUser');
+
+// Error: Course not found
+const courseNotFound = new Error('Course not found.');
+courseNotFound.status = 404;
+
 // ========================================
 // ROUTES
 // ========================================
@@ -32,7 +37,8 @@ router
 
       res.status(200).json(result);
     } catch (error) {
-      res.send(error);
+      // res.send(error);
+      next(error);
     }
   })
   // Create a course
@@ -50,13 +56,15 @@ router
       });
       res.end();
     } catch (error) {
-      res.send(error);
+      // res.send(error);
+      next(error);
     }
   });
 
 // ROUTE: api/courses/#
 router
   .route('/:id')
+  // GET course
   .get(async (req, res, next) => {
     try {
       // course, data includes userId
@@ -64,27 +72,46 @@ router
 
       res.status(200).json(result);
     } catch (error) {
-      res.send(error);
+      // res.send(error);
+      next(error);
     }
   })
+  // UPDATE course
   .put(authenticateUser, async (req, res, next) => {
     try {
       // course
       const course = await Course.findByPk(req.params.id);
       // if there is this course
       if (course) {
+        // IMPORTANT: Sequalize 'update' doesn't reliably validate.
+        // Course needs 'built' and then manually validated
+        // before calling update.
+
+        // DANGER: Don't build an instance with client supplied data
+        // with the setting "isNewRecord: false", and then call save on it.
+        // This can result in the client supplied 'id' number being used
+        // instead of the number set here. (Gaining access to a different record.)
+
+        // Builds the course using client sent data.
+        const updatedCourse = await Course.build(req.body);
+
+        // Validates manually.
+        await updatedCourse.validate();
+
         // Update
         await course.update(req.body);
       } else {
         // no such course
-        res.status(404);
+        next(courseNotFound);
       }
       // Success
       res.status(204).end();
     } catch (error) {
-      res.status(error);
+      // res.status(error);
+      next(error);
     }
   })
+  // DELETE course
   .delete(authenticateUser, async (req, res, next) => {
     try {
       // course
@@ -95,12 +122,16 @@ router
         await course.destroy(req.body);
       } else {
         // no such course
-        res.status(404);
+        next(courseNotFound);
       }
       // Success
       res.status(204).end();
+      // TO DO Does the location header need set to '/'?
+      // It does need to go to the home route
+      // https://app.slack.com/client/TBPQFGEAH/CBPRYGLSZ/thread/CBPRYGLSZ-1563967481.032400
     } catch (error) {
       // res.status(error);
+      next(error);
     }
   });
 

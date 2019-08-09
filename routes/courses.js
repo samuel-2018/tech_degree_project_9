ae95731c8af036d3ddc3cfaaac6889c39f74dbfe
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-08-09 11:39:10 -0400

    Sequelize error handling, sanitize user input

--- routes/courses.js
+++ routes/courses.js
@@ -2,22 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
-// router.use(express.json());
+// Sanitization middlewares
+const { sanitizeBody } = require('express-validator');
+const { sanitizeParam } = require('express-validator');
 
 // Database access
-const { sequelize, models } = require('../db');
+const { models } = require('../db');
 
-const { Course, User } = models;
-
-// Sequelize operators
-const { Op } = sequelize;
+const { Course } = models;
 
 // ========================================
 //  HELPER FUNCTIONS
 // ========================================
-
-// Authenticate user
-const authenticateUser = require('../helpers/authenticateUser');
 
 // Error: Course not found
 const courseNotFound = new Error('Course not found.');
@@ -27,14 +23,18 @@ courseNotFound.status = 404;
 const forbidden = new Error('Forbidden.');
 forbidden.status = 403;
 
+// Authenticate user
+const authenticateUser = require('../helpers/authenticateUser');
+
 // Authenticate access
 const authenticateAccess = async (req, res, next) => {
   try {
     // Get course
     // IMPORTANT: Checks 'id' in params NOT 'id' in client JSON.
+    // (For reliablity and security, use one source of truth for 'id'.)
     const course = await Course.findByPk(req.params.id);
 
-    // if there is this course
+    // Does course exist?
     if (course) {
       // Get current authenticated user
       const user = await req.currentUser.dataValues.id;
@@ -56,14 +56,27 @@ const authenticateAccess = async (req, res, next) => {
     return next(error);
   }
 };
+
 // ========================================
 // ROUTES
 // ========================================
 
-// ROUTE: api/courses
+// Sanitize user input
+// (Will mutate data)
+router.use([
+  sanitizeBody(['title', 'estimatedTime', 'materialsNeeded', 'description'])
+    .trim()
+    .escape(),
+  sanitizeParam(['id'])
+    .trim()
+    .escape(),
+]);
+
+// ROUTE - api/courses
 router
   .route('/')
-  // Get list of courses
+
+  // GET - List of courses
   .get(async (req, res, next) => {
     //
     try {
@@ -74,11 +87,17 @@ router
 
       res.status(200).json(result);
     } catch (error) {
-      // res.send(error);
+      // Catches validation errors sent from Sequelize
+      if (error.name === 'SequelizeValidationError') {
+        // Bad Request
+        // msg assigned by Sequelize
+        error.status = 400;
+      }
       next(error);
     }
   })
-  // Create a course
+
+  // POST - Create a course
   .post(authenticateUser, async (req, res, next) => {
     try {
       // Creates and saves new course
@@ -93,44 +112,64 @@ router
       });
       res.end();
     } catch (error) {
-      // res.send(error);
+      // Catches validation errors sent from Sequelize
+      if (error.name === 'SequelizeValidationError') {
+        // Bad Request
+        // Message assigned by Sequelize
+        error.status = 400;
+      }
       next(error);
     }
   });
 
-// ROUTE: api/courses/#
+// ROUTE - api/courses/#
 router
   .route('/:id')
-  // GET course
+  // GET - One course
   .get(async (req, res, next) => {
     try {
+      console.log(req.params.id);
+
       // course, data includes userId
       const result = await Course.findByPk(req.params.id, {
         attributes: { exclude: ['createdAt', 'updatedAt'] },
       });
-
-      res.status(200).json(result);
+      if (result) {
+        // Returns course
+        res.status(200).json(result);
+      } else {
+        next(courseNotFound);
+      }
     } catch (error) {
-      // res.send(error);
+      // Catches validation errors sent from Sequelize
+      if (error.name === 'SequelizeValidationError') {
+        // Bad Request
+        // msg assigned by Sequelize
+        error.status = 400;
+      }
       next(error);
     }
   })
-  // UPDATE course
+
+  // PUT - Update a course
   .put(authenticateUser, authenticateAccess, async (req, res, next) => {
     try {
-      // course
+      // Get course
+      // IMPORTANT: Gets 'id' in params NOT 'id' in client JSON.
+      // (For reliablity and security, use one source of truth for 'id'.)
       const course = await Course.findByPk(req.params.id);
 
-      // if there is this course
+      // Does course exist?
       if (course) {
         // IMPORTANT: Sequalize 'update' doesn't reliably validate.
         // Course needs 'built' and then manually validated
         // before calling update.
 
         // DANGER: Don't build an instance with client supplied data
-        // with the setting "isNewRecord: false", and then call save on it.
-        // This can result in the client supplied 'id' number being used
-        // instead of the number set here. (Gaining access to a different record.)
+        // with the setting "isNewRecord: false", and then call save.
+        // This can result in the client/JSON supplied course 'id'
+        // number being used instead of the number set here.
+        // (Gaining access to a different record.)
 
         // Builds the course using client sent data.
         const updatedCourse = await Course.build(req.body);
@@ -139,39 +178,55 @@ router
         await updatedCourse.validate();
 
         // Update
+        // (If client supplies an 'id' property in JSON,
+        // it will be safely ignored by update.)
         await course.update(req.body);
 
         // Success
         res.status(204).end();
       } else {
-        // no such course
+        // No such course
         next(courseNotFound);
       }
     } catch (error) {
-      // res.status(error);
+      // Catches validation errors sent from Sequelize
+      if (error.name === 'SequelizeValidationError') {
+        // Bad Request
+        // Message assigned by Sequelize
+        error.status = 400;
+      }
       next(error);
     }
   })
-  // DELETE course
+
+  // DELETE - One course
   .delete(authenticateUser, authenticateAccess, async (req, res, next) => {
     try {
-      // course
+      // Get course
       const course = await Course.findByPk(req.params.id);
-      // if there is this course
+
+      // Does course exist?
       if (course) {
         // Delete
         await course.destroy(req.body);
       } else {
-        // no such course
+        // No such course
         next(courseNotFound);
       }
       // Success
-      res.status(204).end();
-      // TO DO Does the location header need set to '/'?
-      // It does need to go to the home route
-      // https://app.slack.com/client/TBPQFGEAH/CBPRYGLSZ/thread/CBPRYGLSZ-1563967481.032400
+      res.writeHead(204, {
+        // BUG? '/' is the project requirement... but '/api' would be more useful
+        // https://app.slack.com/client/TBPQFGEAH/CBPRYGLSZ/thread/CBPRYGLSZ-1563967481.032400
+        Location: '/',
+      });
+      res.end();
     } catch (error) {
-      // res.status(error);
+      // Catches validation errors sent from Sequelize
+      if (error.name === 'SequelizeValidationError') {
+        // Bad Request
+        // Message assigned by Sequelize
+        error.status = 400;
+      }
       next(error);
     }
   });
